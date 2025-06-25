9d714e97d9dc88ba2fd406af83aafe81e05efa8a
unknown <jtf-charles>
2025-06-25 06:07:17 -0500

    completed lab

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <div id="links">
+      <h3>Links</h3>
+      <a href={props.links.github}>{props.links.github}</a><br />
+      <a href={props.links.linkedin}>{props.links.linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
